1a8254af71e5c68ce5b71e6983ed58005ab92643
Tim Hanewich <tahanewich@live.com>
2021-05-17 09:58:40 -0400

    Prepared program for testing

--- testing/Program.cs
+++ testing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TimHanewich.ImageMetadata;
 
 namespace testing
 {
@@ -6,7 +7,9 @@ namespace testing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            
+
         }
     }
 }
